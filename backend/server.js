b60c7e1cfd59afae22324dccd18908d5a2c08df8
Krishna3558 <rajeevmishra6066@gmail.com>
2025-05-29 19:57:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const server = app.listen(PORT , () => {
 const io = require('socket.io')(server , {
     pingTimeout:60000,
     cors: {
-        origin: "http://localhost:5173"
+        origin: "https://chatapp-frontend-wj6k.onrender.com"
     }
 });
 
